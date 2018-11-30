23b1974d7643576fef9ddc4e2885a1572debc546
kasptom <kasptom@gmail.com>
2018-11-30 23:09:45 +0100

    Initialize repo, add an examplary listener / talker

---
+++ topnav_driving_strategies/run_talker.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Warning JAVA_HOME should use jdk8
+rosrun topnav_rosjava_kasptom topnav_driving_strategies com.github.rosjava.topnav_rosjava_kasptom.topnav_driving_strategies.Talker
