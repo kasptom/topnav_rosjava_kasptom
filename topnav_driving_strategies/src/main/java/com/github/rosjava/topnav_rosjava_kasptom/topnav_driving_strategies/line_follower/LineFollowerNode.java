33e2d7ba5923837a61c9267b8a76a4b72a5aea0a
kasptom <kasptom@gmail.com>
2018-12-02 22:43:42 +0100

    Add wheels joint state publisher
          * FIXME header seems to be malformed
          header:
            seq: 231134
            stamp:
              secs: 0
              nsecs:         0
            frame_id: ''
          name: [front_left_wheel_joint, rear_right_wheel_joint, rear_left_wheel_joint, front_right_wheel_joint]
          position: []
          velocity: [2.0, 2.0, 2.0, 2.0]
          effort: []

--- topnav_driving_strategies/src/main/java/com/github/rosjava/topnav_rosjava_kasptom/topnav_driving_strategies/line_follower/LineFollowerNode.java
+++ topnav_driving_strategies/src/main/java/com/github/rosjava/topnav_rosjava_kasptom/topnav_driving_strategies/line_follower/LineFollowerNode.java
@@ -1,6 +1,5 @@
 package com.github.rosjava.topnav_rosjava_kasptom.topnav_driving_strategies.line_follower;
 
-import org.apache.commons.logging.Log;
 import org.ros.namespace.GraphName;
 import org.ros.node.AbstractNodeMain;
 import org.ros.node.ConnectedNode;
@@ -8,7 +7,9 @@ import org.ros.node.topic.Subscriber;
 import topnav_msgs.AngleRangesMsg;
 
 
+@SuppressWarnings("unused")
 public class LineFollowerNode extends AbstractNodeMain {
+
     @Override
     public GraphName getDefaultNodeName() {
         return GraphName.of("topnav/driving_strategies/line_follower");
@@ -16,7 +17,10 @@ public class LineFollowerNode extends AbstractNodeMain {
 
     @Override
     public void onStart(ConnectedNode connectedNode) {
-        final Log log = connectedNode.getLog();
-        Subscriber<AngleRangesMsg> subscriber = connectedNode.newSubscriber("angle_range", AngleRangesMsg._TYPE);
+        Subscriber<AngleRangesMsg> angleRangesMsgSubscriber = connectedNode.newSubscriber("capo/laser/angle_range", AngleRangesMsg._TYPE);
+        angleRangesMsgSubscriber.addMessageListener(
+                new WheelsJointStatePublisher("joint_states", connectedNode)
+//                new WheelsJointStatePublisher("capo_diff_drive_controller/cmd_vel", connectedNode)
+        );
     }
 }
