eeafc2f904febd07918f6149dd697df1c61d3f90
kasptom <kasptom@gmail.com>
2019-01-07 00:58:55 +0100

    Log position of each detected marker at most 1 time per second

--- topnav_driving_strategies/src/main/java/com/github/rosjava/topnav_rosjava_kasptom/topnav_driving_strategies/navigation/MarkerMessageHandler.java
+++ topnav_driving_strategies/src/main/java/com/github/rosjava/topnav_rosjava_kasptom/topnav_driving_strategies/navigation/MarkerMessageHandler.java
@@ -1,8 +1,6 @@
 package com.github.rosjava.topnav_rosjava_kasptom.topnav_driving_strategies.navigation;
 
 import com.github.rosjava.topnav_rosjava_kasptom.topnav_driving_strategies.models.MarkerDetection;
-import com.github.rosjava.topnav_rosjava_kasptom.topnav_driving_strategies.models.RelativeAlignment;
-import com.github.rosjava.topnav_rosjava_kasptom.topnav_driving_strategies.models.RelativeDistance;
 import org.ros.message.MessageListener;
 import topnav_msgs.MarkerMsg;
 import topnav_msgs.MarkersMsg;
@@ -10,9 +8,7 @@ import topnav_msgs.MarkersMsg;
 import java.util.HashMap;
 
 public class MarkerMessageHandler implements MessageListener<MarkersMsg> {
-
-    private HashMap<String, RelativeAlignment> previousAlignments = new HashMap<>();
-    private HashMap<String, RelativeDistance> previousDistances = new HashMap<>();
+    private HashMap<String, Long> previousTimeStamp = new HashMap<>();
 
     @Override
     public void onNewMessage(MarkersMsg markersMsg) {
@@ -22,15 +18,11 @@ public class MarkerMessageHandler implements MessageListener<MarkersMsg> {
     private void printMarkerMessage(MarkerMsg markerMsg) {
         double[] pos = markerMsg.getCameraPosition();
         MarkerDetection detection = MarkerDetection.createDetection(Integer.toString(markerMsg.getId()), markerMsg.getCameraPosition());
+        Long currentTimeStamp = System.nanoTime();
 
-        RelativeAlignment currentAlignment = detection.getRelativeAlignment();
-        RelativeDistance currentDistance = detection.getRelativeDistance();
-
-        if (currentAlignment != previousAlignments.get(detection.getId())
-                || currentDistance != previousDistances.get(detection.getId())) {
-            previousAlignments.put(detection.getId(), currentAlignment);
-            previousDistances.put(detection.getId(), currentDistance);
-
+        if (!previousTimeStamp.containsKey(detection.getId())
+                || currentTimeStamp - previousTimeStamp.get(detection.getId()) > 1e9) {
+            previousTimeStamp.put(detection.getId(), currentTimeStamp);
             System.out.printf("Marker ID: %s\n Position: (%.2f, %.2f, %.2f)\n %s %s\n",
                     detection.getId(), pos[0], pos[1], pos[2], detection.getRelativeAlignment(), detection.getRelativeDistance());
         }
