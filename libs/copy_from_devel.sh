ead9e490a8687375dcdbdf6274570aa75967c6a3
kasptom <kasptom@gmail.com>
2019-08-08 19:48:00 +0200

    Add the ssh upload scripts

---
+++ libs/copy_from_devel.sh
@@ -0,0 +1,2 @@
+cp $HOME/Dev/tnavbot_ws/devel/share/maven/org/ros/rosjava_messages/topnav_msgs/0.0.0/topnav_msgs-0.0.0.pom .
+cp $HOME/Dev/tnavbot_ws/devel/share/maven/org/ros/rosjava_messages/topnav_msgs/0.0.0/topnav_msgs-0.0.0.jar .
---
+++ libs/upload_to_capo2.sh
@@ -0,0 +1,3 @@
+ssh capo2@192.168.1.108 'mkdir -p /home/capo2/Dev/tnavbot_ws/devel/share/maven/org/ros/rosjava_messages/topnav_msgs/0.0.0/'
+scp $HOME/Dev/tnavbot_ws/devel/share/maven/org/ros/rosjava_messages/topnav_msgs/0.0.0/topnav_msgs-0.0.0.pom capo2@192.168.1.108:/home/capo2/Dev/tnavbot_ws/devel/share/maven/org/ros/rosjava_messages/topnav_msgs/0.0.0/
+scp $HOME/Dev/tnavbot_ws/devel/share/maven/org/ros/rosjava_messages/topnav_msgs/0.0.0/topnav_msgs-0.0.0.jar capo2@192.168.1.108:/home/capo2/Dev/tnavbot_ws/devel/share/maven/org/ros/rosjava_messages/topnav_msgs/0.0.0/
