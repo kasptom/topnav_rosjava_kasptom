c559367a145242121241893805631b789ca91930
Tomasz Kasprzyk <kasptom@users.noreply.github.com>
2019-08-17 13:41:53 +0200

    Increase the aruco preview resolution (#27)

--- topnav_shared/src/main/java/com/github/topnav_rosjava_kasptom/topnav_shared/constants/Preview.java
+++ topnav_shared/src/main/java/com/github/topnav_rosjava_kasptom/topnav_shared/constants/Preview.java
@@ -1,10 +1,10 @@
 package com.github.topnav_rosjava_kasptom.topnav_shared.constants;
 
 public class Preview {
-//    public static int CAM_PREVIEW_WIDTH = 640;
-//    public static int CAM_PREVIEW_HEIGHT = 480;
-    public static int CAM_PREVIEW_WIDTH = 320;
-    public static int CAM_PREVIEW_HEIGHT = 240;
+    public static int CAM_PREVIEW_WIDTH = 640;
+    public static int CAM_PREVIEW_HEIGHT = 480;
+//    public static int CAM_PREVIEW_WIDTH = 320;
+//    public static int CAM_PREVIEW_HEIGHT = 240;
 
     public static int LIDAR_PREVIEW_WIDTH = 480;
     public static int LIDAR_PREVIEW_HEIGHT = 480;
